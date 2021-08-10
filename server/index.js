0a54fa12ae00929a1b012bc31e66155e3b0d29ff
ChaarviRastogi22 <chaarvirastogi190@gmail.com>
2021-08-10 15:42:10 +0530

    Server running

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
+// Importing Env Variables
+require("dotenv").config();
+
+// Libraries
 import express from "express";
 import cors from "cors"; //cross origin request
 import helmet from "helmet";
 
+// Database connection
+import ConnectDB from "./database/connection";
 
 const zomato = express();
 
@@ -13,4 +19,7 @@ zomato.use(cors());
 
 zomato.get("/", (req, res) => res.json({message: "Setup success"}));
 
-zomato.listen(4000, () => console.log("Server is running fine🎉"));
+zomato.listen(4000, () => 
+            ConnectDB().then(() => console.log("Server is running fine🎉"))
+            .catch(() => console.log("Server is running but database connection failed!!"))
+);
