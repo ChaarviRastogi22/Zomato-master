8534bdd6f6fe4e5d8b70ded48106f18e2ced0186
ChaarviRastogi22 <chaarvirastogi190@gmail.com>
2021-08-11 16:04:55 +0530

    added google auth

--- server/database/user/index.js
+++ server/database/user/index.js
@@ -19,7 +19,6 @@ UserSchema.methods.generateJwtToken = function () {
   return jwt.sign({ user: this._id.toString() }, "ZomatoAPP");
 };
 
-
 UserSchema.statics.findByEmailAndPhone = async ({ email, phoneNumber }) => {
   // check whether email exist
   const checkUserByEmail = await UserModel.findOne({ email });
@@ -32,6 +31,18 @@ UserSchema.statics.findByEmailAndPhone = async ({ email, phoneNumber }) => {
   return false;
 };
 
+UserSchema.statics.findByEmailAndPassword = async ({ email, password }) => {
+  // check whether email exist
+  const user = await UserModel.findOne({ email });
+  if (!user) throw new Error("User does no exist!!!");
+
+  // Compare password
+  const doesPasswordMatch = await bcrypt.compare(password, user.password);
+
+  if (!doesPasswordMatch) throw new Error("invalid Password!!!");
+
+  return user;
+};
 
 UserSchema.pre("save", function (next) {
   const user = this;
